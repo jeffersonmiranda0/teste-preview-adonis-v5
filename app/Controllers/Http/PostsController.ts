8cd1d646ecd5680381d0c0b27d386dc41daa3bcd
Jefferson Miranda <jeffersonmiranda0@gmail.com>
2020-03-21 00:12:31 -0300

    Teste da versão 5 do adonis

---
+++ app/Controllers/Http/PostsController.ts
@@ -0,0 +1,17 @@
+import Post from "App/Models/Post";
+import { HttpContextContract } from "@ioc:Adonis/Core/HttpContext";
+
+export default class PostsController {
+  public async index() {
+    const posts = await Post.all();
+    return posts;
+  }
+
+  public async store({ request }: HttpContextContract) {
+    const data = request.only(["title", "content"]);
+
+    const post = await Post.create(data);
+
+    return post;
+  }
+}
